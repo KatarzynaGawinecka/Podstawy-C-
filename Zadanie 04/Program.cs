b7b92aa485e16296de9f991ada066eac71f8d107
KatarzynaGawinecka <katarzyna.gawinecka.936@studms.ug.edu.pl>
2025-02-28 13:10:28 +0100

    koniec zajęć

---
+++ Zadanie 04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący boki prostokąta i wypisujący na ekranie jego pole i obwód. 
+            double a, b;
+            Console.Write("Podaj bok a=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj bok b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Pole to a*b={0}", a * b);
+            Console.WriteLine("Obwód to 2*a+2*b={0}", 2 * a + 2 * b);
+            Console.ReadKey(true); //pauza
+   
+        }
+    }
+}
---
+++ Zadanie 05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący promień podstawy (r) i wysokość (h) stożka i wypisujący na ekranie objętość i pole całkowite tego stożka.
+            Double r, h, l, V, Pc;
+            Console.Write("Podaj promień podstawy stożka r=");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wysokość stożka h=");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Objętość stożka to");
+            l = Math.Sqrt(r * r + h * h);
+            V = 1.0 / 3 * Math.PI * r * r * h;
+            Pc = Math.PI * r * r + Math.PI * r * l;
+            Console.WriteLine("Objętość stożka wynosi V={0}", V);
+            Console.WriteLine("Pole całkowite stożka wynosi Pc={0}", Pc);
+            Console.ReadKey(true); //pauza
+
+        }
+    }
+}
